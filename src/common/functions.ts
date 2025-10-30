185c7d563d01a4410062bc738a1097d7036dde88
mrchenguozheng <30428638+mrchenguozheng@users.noreply.github.com>
2025-10-30 21:10:13 +0800

    update: support convertUnspacedPinyin

--- src/common/functions.ts
+++ src/common/functions.ts
@@ -1,7 +1,8 @@
 import toPinyinTonesV1 from 'pinyin-tone'
-import toPinyinTonesV2 from 'pinyin-tone/v2'
+import toPinyinTonesV2, {convertUnspacedPinyin} from 'pinyin-tone/v2'
 
 export const toPinyin = {
     v1: toPinyinTonesV1,
     v2: toPinyinTonesV2,
+    convertUnspacedPinyin: convertUnspacedPinyin,
 }
